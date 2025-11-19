ef4e5bb042f9c586a8f740ea7ae6f73337e51032
ankhemanta <ankhemanta@gmail.com>
2025-11-19 11:10:22 +0530

    add generatRandomText randomNumberInRange genRandomKey generatRandomUserId etc add

---
+++ generator/number.generator.js
@@ -0,0 +1,57 @@
+
+
+
+export const generatRandomUserId = (size) => {
+  let num = 0;
+  var numStr = num.toString();
+  while (numStr.length < size) {
+    numStr = 0 + numStr;
+  }
+  let a = 1000;
+  let b = parseInt(1 + numStr);
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+};
+
+export const generatRandomId = (size) => {
+  let num = 0;
+  var numStr = num.toString();
+  while (numStr.length < size) {
+    numStr = 0 + numStr;
+  }
+  let a = 1000;
+  let b = parseInt(1 + numStr);
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+};
+
+
+
+//  if you need to randomn number 
+export const randomNumberInRange = (min, max) => {
+  return Math.floor(Math.random()
+    * (max - min + 1)) + min;
+};
+
+
+//IF YOU NEED TO RANDOM PATH OR TEXT 
+export function generatRandomText(string_length) {
+  var random_string = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqurstuvwxyz'
+  for (var i = 0; i < string_length; i++) {
+    random_string += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return random_string
+};
+
+
+//IF YOU NEED TO RANDOM PATH OR TEXT WITH NUMBER 
+export function genRandomKey(string_length) {
+  var random_string = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqurstuvwxyz-_12345690';
+
+  for (var i = 0; i < string_length; i++) {
+    random_string += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return random_string
+};
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+import {
+  generatRandomUserId,
+  generatRandomId,
+  randomNumberInRange,
+  generatRandomText,
+  genRandomKey
+} from './generator/number.generator.js';
+
+
+
+
+export {
+  generatRandomUserId,
+  generatRandomId,
+  randomNumberInRange,
+  generatRandomText,
+  genRandomKey,
+
+  generatRandomUserId as getRandomUserId,
+  generatRandomId as getRandomId,
+  randomNumberInRange as getNumInRange,
+  generatRandomText as getRandomText,
+  genRandomKey as getRamdomKey
+}
